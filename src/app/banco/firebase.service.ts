72b163adde634c58f71200b0f9eb633dc1fd79c0
Jimi Togni <jimitogni@yahoo.com.br>
2019-03-14 16:26:16 -0300

    codigo comentado

--- src/app/banco/firebase.service.ts
+++ src/app/banco/firebase.service.ts
@@ -3,6 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+//interface do nosso objeto produto, esses serão os atributos gravados no banco de dados do Firebase da google
 export interface Produto {
   id?: string;
   nome: string;
@@ -16,13 +17,20 @@ export interface Produto {
 })
 
 export class FirebaseService {
+
+  //aqui criamos uma variabel produtosColecao que recebe uma coleção de OBJETOS do bando de dados do fire base
   private produtosColecao: AngularFirestoreCollection<Produto>;
 
+  //aqui criamos uma variabvel produtos que recebe um metodo que "observa" todas as mudanças que forem feitas no objeto Produto
   private produtos: Observable<Produto[]>;
 
+  //tudo o que estiver dentro do construtor será executado automaticamente quando a classe for instanciada(criada)
   constructor(db: AngularFirestore) {
+    
+    //aqui estamos dizendo que a variavel produtosColecao recebe um objeto do AngularFirestore
     this.produtosColecao = db.collection<Produto>('produtos');
 
+    //aqui nós pegamos todos os dados recebidos pela variavel acima "produtosColecao" e extraimos todos os registros que foram retornado pelo banco de dados 
     this.produtos = this.produtosColecao.snapshotChanges().pipe(
       map(actions => {
         return actions.map(e => {
@@ -40,18 +48,22 @@ export class FirebaseService {
     return this.produtos;
   }
 
+  //seleciona apenas UM registro ESPECIFICO pelo ID passado como parametro
   selecionaUm(id) {
     return this.produtosColecao.doc<Produto>(id).valueChanges();
   }
 
+  //atualizamos um registro no banco de dados
   atualizarDados(produto: Produto, id: string) {
     return this.produtosColecao.doc(id).update(produto);
   }
 
+  //inserindo um novo registo no bando
   insereRegisro(produto: Produto) {
     return this.produtosColecao.add(produto);
   }
 
+  //apaga
   apagaRegistro(id) {
     return this.produtosColecao.doc(id).delete();
   }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,33 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Produto, FirebaseService } from '../services/firebase.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  nome: string="";
-  fabricante = "";
-  modelo="";
-  quantidade: number ="";
+  //aqui estamos criando a variavel com o nome de "produto" que recebe uma interface do tipo Produto que foi criado no firebase
+  //e inicializando com valores "vazios e nulos"
+       /*export interface Produto {
+          id?: string;
+          nome: string;
+          modelo: string;
+          preco: number;
+          quantidade: number;
+        }*/  
+  produto: Produto = {
+    nome: '',
+    modelo: '',
+    quantidade: null ,
+    preco: null,
+  }
 
-  ola = "Ola ";
-  var1 = "";
-  var2 = "";
+  produtoId = null;
 
-  cliqueAqui(){
+  //sempre que essa classe "Tab2Page" for instanciada(criada) tudo o que estiver dentro do construtor será executado automáticamente
+  constructor(private route: ActivatedRoute, 
+              private nav: NavController, 
+              private firebaseServer: FirebaseService, 
+              private loadingController: LoadingController) { }
+  //fim construtor
 
-    if (this.nome){
-      alert(this.ola + this.nome);
-      this.var1 = "Bem vindo " + this.nome ;
+    //ngOnInit tudo o que estiver dentro deste método será executado quando a TELA desta classe for aberta pelo usu
+    ngOnInit() {
+      this.produtoId = this.route.snapshot.params['id'];
+      if (this.produtoId)  {
+        this.getTodos();
+      }
     }
 
-    else if (!this.nome){
-      alert("em branco");
-      this.var1 = "variavel em branco";
+    //uma função asincrona é uma função que depende da resposta de algum serviço,
+    //por exemplo API do firebase, ou APi dos correios, google maps, etc
+    //caso o serviço demora muito para responder esse método permite que a tela do aplicativo não fique parada ou congelada
+    //por sua vez o método await loading.present(); exibe um popup na 
+    //tela do celular dizendo que a aplicação está esperando a resposta do serviço
+    //um função assincrona sempre é usado quando nós não temos controle total 
+    //do comportamento do método, como neste caso, dependemos das respostas da API do firebase
+    //o método getTodos faz uma requisição de todos os registros existentes na API do firebase 
+    async getTodos() {
+      const loading = await this.loadingController.create({
+        message: 'Carregando produto..'
+      });
+      await loading.present();
+
+      this.firebaseServer.getUm(this.produtoId).subscribe(res => {
+        loading.dismiss();
+        this.produto = res;
+      });
+    }
+
+
+    async salvaProduto() {
+      const loading = await this.loadingController.create({
+        message: 'Salvando produto..'
+      });
+      await loading.present();
+
+      if (this.produtoId) {
+        this.firebaseServer.update(this.produto, this.produtoId).then(() => {
+          loading.dismiss();
+          this.nav.navigateForward('/tab3');
+        });
+      } else {
+        this.firebaseServer.add(this.produto).then(() => {
+          loading.dismiss();
+          this.nav.navigateForward('/tab3');
+        });
+      }
     }
 
   }
-
-}
